a38aaea9be8ecce00ea50ec4540b661757749df3
Ben Costolo <ben.costolo@gmail.com>
2017-12-21 16:01:36 -0500

    wip: add try-require, conditions for data to be displayed if .coinmon.json is present

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const Table = require('cli-table2')
 const colors = require('colors')
 const humanize = require('humanize-plus')
 const os = require('os')
-const coins = require(`${os.homedir()}/.coinmon.json`)
+const tryRequire = require('try-require')
+const coins = tryRequire(`${os.homedir()}/.coinmon.json`)
 
 const platform = os.platform() // linux, darwin, win32, sunos
 const supportEmoji = platform !== 'darwin'
@@ -29,6 +30,9 @@ if (availableCurrencies.indexOf(convert) === -1) {
 const find = program.find
 const top = !isNaN(program.top) && +program.top > 0 ? +program.top : (find.length > 0 ? 1500 : 10)
 const humanizeIsEnabled = program.humanize !== 'false'
+const head = coins
+  ? ['Rank', 'Coin', `Price (${convert})`, 'Change (24H)', 'Change (1H)', `Market Cap (${convert})`, 'Your Value'].map(title => title.yellow)
+  : ['Rank', 'Coin', `Price (${convert})`, 'Change (24H)', 'Change (1H)', `Market Cap (${convert})`].map(title => title.yellow)
 const table = new Table({
   chars: {
     'top': '-',
@@ -47,7 +51,7 @@ const table = new Table({
     'right-mid': '-',
     'middle': '│'
   },
-  head: ['Rank', 'Coin', `Price (${convert})`, 'Change (24H)', 'Change (1H)', `Market Cap (${convert})`, 'Your Value'].map(title => title.yellow),
+  head,
   colWidths: [6, 14, 15, 15, 15, 20]
 })
 
@@ -64,46 +68,57 @@ cfonts.say('coinmon', {
 const spinner = ora('Loading data').start()
 const sourceUrl = `https://api.coinmarketcap.com/v1/ticker/?limit=${top}&convert=${convert}`
 axios.get(sourceUrl)
-.then(function (response) {
-  spinner.stop()
-  response.data
-    .filter(record => {
-      if (find.length > 0) {
-        return find.some(keyword => record.symbol.toLowerCase() === keyword.toLowerCase())
-      }
-      return true
-    })
-    .map(record => {
-      const percentChange24h = record.percent_change_24h
-      const textChange24h = `${percentChange24h}%`
-      const change24h = percentChange24h? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA'
-      const percentChange1h = record.percent_change_1h
-      const textChange1h = `${percentChange1h}%`
-      const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
-      const marketCap = record[`market_cap_${convert}`.toLowerCase()]
-      const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap
-      const personalValue = coins[record.id]
-        ? `$${(coins[record.id] * record.price_usd).toFixed(2)}`
-        : '$0.00'
-      return [
-        record.rank,
-        `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
-        record[`price_${convert}`.toLowerCase()],
-        change24h,
-        change1h,
-        displayedMarketCap,
-        personalValue
-      ]
-    })
-    .forEach(record => table.push(record))
-  if (table.length === 0) {
-    console.log('We are not able to find coins matching your keywords'.red)
-  } else {
-    console.log(`Data source from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
-    console.log(table.toString())
-  }
-})
-.catch(function (error) {
-  spinner.stop()
-  console.error('Coinmon is not working now. Please try again later.'.red)
-})
+  .then(function (response) {
+    spinner.stop()
+    response.data
+      .filter(record => {
+        if (find.length > 0) {
+          return find.some(keyword => record.symbol.toLowerCase() === keyword.toLowerCase())
+        }
+        return true
+      })
+      .map(record => {
+        const percentChange24h = record.percent_change_24h
+        const textChange24h = `${percentChange24h}%`
+        const change24h = percentChange24h? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA'
+        const percentChange1h = record.percent_change_1h
+        const textChange1h = `${percentChange1h}%`
+        const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
+        const marketCap = record[`market_cap_${convert}`.toLowerCase()]
+        const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap
+        const personalValue = coins && coins[record.id]
+          ? `$${(coins[record.id] * record.price_usd).toFixed(2)}`
+          : '$0.00'
+        const tableData = coins
+          ? [
+            record.rank,
+            `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
+            record[`price_${convert}`.toLowerCase()],
+            change24h,
+            change1h,
+            displayedMarketCap,
+            personalValue
+          ]
+          : [
+            record.rank,
+            `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
+            record[`price_${convert}`.toLowerCase()],
+            change24h,
+            change1h,
+            displayedMarketCap,
+          ]
+
+          return tableData;
+      })
+      .forEach(record => table.push(record))
+    if (table.length === 0) {
+      console.log('We are not able to find coins matching your keywords'.red)
+    } else {
+      console.log(`Data source from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
+      console.log(table.toString())
+    }
+  })
+  .catch(function (error) {
+    spinner.stop()
+    console.error('Coinmon is not working now. Please try again later.'.red)
+  })
